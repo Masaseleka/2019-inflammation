a719ce9cc770bb22e5a585fb2deef19249875964
Masaseleka <55438259+Masaseleka@users.noreply.github.com>
2019-09-17 12:35:38 +0200

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -1,2 +1,14 @@
-to have a py formula
 
+import numpy as np
+data = np.loadtxt(fname='inflammation-01.csv', delimiter=',')
+import matplotlib.pyplot as plt
+ave_inflammation=np.mean(
+  data,
+  axis=0
+)
+plt.plot(ave_inflammation)
+max_inlammation=p.max(
+  data,
+  axis=0
+)
+plt.plot(max_inflammation)
